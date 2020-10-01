ca63663754a2d3b1cf2436fa369cfe1617a7b03d
Thaylor Carvalho <thaylorcs@outlook.com>
2020-10-01 13:15:45 -0300

    JS Básico

---
+++ JS/js/main.js
@@ -0,0 +1,60 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    
+}
+
+function redirecionar(){
+    window.open("https://google.com");
+}
+
+
+/* function soma(n1, n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    } else {
+        validar = false;
+    }
+
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+    console.log(validaIdade(idade));
+ */
+/* var d = new Date();
+
+alert(d.getDate());
+ */
+
+/* var count = 5;
+while(count < 5){
+    console.log(count);
+    count++;
+};
+ */
+/* var idade = prompt("Qual é a sua idade?");
+
+if(idade >= 18 ){
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+} */
+
+/* var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[1].cor); */
+
+/* var nome = "Thaylor Carvalho";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+alert("Seja bem vindo, " + nome); */
