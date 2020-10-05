34d6f36b2f03aabaa43f4481d8415c108ac26ac8
Thaylor Carvalho <thaylorcs@outlook.com>
2020-10-05 20:07:26 -0300

    Detectando colisão

--- snake_js/js/script.js
+++ snake_js/js/script.js
@@ -45,7 +45,13 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
-    
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over");
+        }
+    }
 
     criarBg();
     criarCobra();
@@ -65,9 +71,7 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random() * 15 + 1) * box,
         food.y = Math.floor(Math.random() * 15 + 1) * box 
 
-    }
-
-    
+    }    
 
     let newHead = {
         x: snakeX ,
