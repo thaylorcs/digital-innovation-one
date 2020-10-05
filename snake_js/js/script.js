1d606e0d7c395cdfb3edbc50f6c9519accae6d71
Thaylor Carvalho <thaylorcs@outlook.com>
2020-10-05 18:18:46 -0300

    Criar movimento

--- snake_js/js/script.js
+++ snake_js/js/script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBg(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,5 +21,28 @@ function criarCobra(){
     }
 }
 
-criarBg();
-criarCobra();
+function iniciarJogo(){
+    criarBg();
+    criarCobra();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX ,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
